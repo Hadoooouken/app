cccd97c799154c802af30b54fb363245836d0e18
Filipp <wolverinexdd@yandex.ru>
2026-03-09 20:07:16 +0400

    добавлены новые СВГ

--- planner/furniture-catalog.js
+++ planner/furniture-catalog.js
@@ -1,26 +1,76 @@
 // world units у тебя = cm, значит 200см = 200
+// export const FURN_CATEGORIES = [
+//     {
+//         id: 'living',
+//         label: 'Гостиная/спальня',
+//         items: [
+//             { typeId: 'sofa', label: 'Диван', symbolId: 'mebel-divan-big', w: 200, h: 100 },
+//             { typeId: 'bed', label: 'Кровать', symbolId: 'bed-big', w: 200, h: 160 },
+//         ],
+//     },
+//     {
+//         id: 'kitchen',
+//         label: 'Кухня',
+//         items: [
+//             { typeId: 'kitchen', label: 'Гарнитур', symbolId: 'f-kitchen', w: 240, h: 60 },
+//         ],
+//     },
+//     {
+//         id: 'bath',
+//         label: 'Ванная',
+//         items: [
+//             { typeId: 'toilet', label: 'Унитаз', symbolId: 'f-toilet', w: 70, h: 40 },
+//             { typeId: 'bath', label: 'Ванна', symbolId: 'f-bath', w: 170, h: 70 },
+//         ],
+//     },
+// ]
+
+// world units у тебя = cm, значит 200см = 200
 export const FURN_CATEGORIES = [
     {
         id: 'living',
         label: 'Гостиная/спальня',
         items: [
-            { typeId: 'sofa', label: 'Диван', symbolId: 'mebel-divan-big', w: 200, h: 100 },
-            { typeId: 'bed', label: 'Кровать', symbolId: 'bed-big', w: 200, h: 160 },
+            { typeId: 'sofa-small', label: 'Диван малый', symbolId: 'mebel-divan-small', w: 150, h: 82 },
+            { typeId: 'sofa-medium', label: 'Диван средний', symbolId: 'mebel-divan-normal', w: 200, h: 82 },
+            { typeId: 'sofa-big', label: 'Диван большой', symbolId: 'mebel-divan-big', w: 250, h: 82 },
+
+            { typeId: 'bed-double', label: 'Кровать двуспальная', symbolId: 'mebel-krovat-big', w: 160, h: 200 },
+            { typeId: 'bed-single', label: 'Кровать односпальная', symbolId: 'mebel-krovat-small', w: 90, h: 200 },
+            { typeId: 'bedside-table', label: 'Прикроватная тумба', symbolId: 'mebel-tumba', w: 50, h: 45 },
+
+            { typeId: 'desk', label: 'Рабочее место', symbolId: 'mebel-komp-stol', w: 130, h: 110 },
+            { typeId: 'coffee-table', label: 'Журнальный стол', symbolId: 'mebel-jurn-stol', w: 90, h: 50 },
+            { typeId: 'tv', label: 'Телевизор', symbolId: 'mebel-tv', w: 110, h: 10 },
+
+            { typeId: 'wardrobe-1', label: 'Шкаф 1м', symbolId: 'mebel-skaf-1', w: 100, h: 54 },
+            { typeId: 'wardrobe-2', label: 'Шкаф 2м', symbolId: 'mebel-skaf-2', w: 200, h: 54 },
+            { typeId: 'wardrobe-3', label: 'Шкаф 3м', symbolId: 'mebel-skaf-3', w: 300, h: 54 },
         ],
     },
+
     {
         id: 'kitchen',
         label: 'Кухня',
         items: [
-            { typeId: 'kitchen', label: 'Гарнитур', symbolId: 'f-kitchen', w: 240, h: 60 },
+            { typeId: 'kitchen-2', label: 'Кухня 2м', symbolId: 'mebel-kuhnya-small', w: 200, h: 60 },
+            { typeId: 'kitchen-2-5', label: 'Кухня 2.5м', symbolId: 'mebel-kuhnya-normal', w: 240, h: 60 },
+            { typeId: 'kitchen-3', label: 'Кухня 3м', symbolId: 'mebel-kuhnya-big', w: 300, h: 60 },
+
+            { typeId: 'table-small', label: 'Кухонный стол малый', symbolId: 'mebel-stol-small', w: 80, h: 60 },
+            { typeId: 'table-medium', label: 'Кухонный стол средний', symbolId: 'mebel-stol-normal', w: 120, h: 70 },
+            { typeId: 'table-big', label: 'Кухонный стол большой', symbolId: 'mebel-stol-big', w: 160, h: 90 },
         ],
     },
+
     {
         id: 'bath',
         label: 'Ванная',
         items: [
-            { typeId: 'toilet', label: 'Унитаз', symbolId: 'f-toilet', w: 70, h: 40 },
-            { typeId: 'bath', label: 'Ванна', symbolId: 'f-bath', w: 170, h: 70 },
+            { typeId: 'toilet', label: 'Унитаз', symbolId: 'mebel-unitaz', w: 36, h: 62 },
+            { typeId: 'bath', label: 'Ванна', symbolId: 'mebel-vanna', w: 180, h: 80 },
+            { typeId: 'sink', label: 'Раковина', symbolId: 'mebel-rakovina', w: 66, h: 50 },
+            { typeId: 'shower', label: 'Душевая кабина', symbolId: 'mebel-kabina', w: 90, h: 90 },
         ],
     },
 ]
